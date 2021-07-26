a1727686d9736a3a76d1584ca794d098ae8692e1
harzh307 <harzh.patel@gmail.com>
2021-07-26 17:06:31 +0530

    projects page done

--- container/SinglePortfolio/index.js
+++ container/SinglePortfolio/index.js
@@ -1,48 +1,84 @@
 import { useRouter } from 'next/router';
-import Image from 'next/image';
-import Head from 'next/head';
+// import { useCallback, useEffect, useRef } from 'react';
+import Button from '../../component/Button';
 import Header from '../../component/Header';
 import PortText from '../../component/PortText';
-// import Contact from '../../container/Contact';
-import Button from '../../component/Button';
+// import'from''./portfolio.scss';
 
 const Project = ({ data }) => {
   const router = useRouter();
+  // const imgRef = useRef(null);
+  // const intervalRef = useRef();
+  // const cardRef = useRef();
+
+  // const startScroll = useCallback(() => {
+  //   intervalRef.current = setInterval(() => {
+  //     const total = imgRef.current.scrollLeft + imgRef.current.offsetWidth;
+  //     if (Math.round(total) === imgRef.current.scrollWidth) {
+  //       imgRef.current.scrollLeft = 0;
+  //     } else {
+  //       imgRef.current.scrollLeft += cardWidth;
+  //     }
+  //   }, 3000);
+  // }, []);
+
+  // const stopScroll = useCallback(() => {
+  //   clearInterval(intervalRef.current);
+  // }, []);
+
+  // useEffect(() => {
+  //   startScroll();
+  //   imgRef.current.addEventListener('mouseover', stopScroll);
+  //   imgRef.current.addEventListener('mouseout', startScroll);
+  //   return () => {
+  //     imgRef.current.removeEventListener('mouseover', stopScroll);
+  //     imgRef.current.removeEventListener('mouseout', startScroll);
+  //   };
+  // }, [startScroll, stopScroll]);
 
   return (
-    // const { data: portfolioData } = usePortfolio();
     <>
       <div className="light_theme">
         <Header>
           <PortText variant="portSingleBlog" className="portSingleBlog">
             Project Details
           </PortText>
-          <a className="backAlign" aria-label="back" role="button" onClick={() => router.back()}>
+          <button
+            className="backAlign"
+            type="button"
+            onClick={() => {
+              router.back();
+            }}>
             Back
-          </a>
+          </button>
         </Header>
         <div className="mainDiv">
           <div className="imgdiv">
+            {/* <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" />
+            <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" />
+            <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" />
+            <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" />
+            <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" />
+            <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" />
+            <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" /> */}
             {data?.media?.map(x => (
               <img src={x?.url} className="img" alt="" />
             ))}
           </div>
           <div className="textDiv">
             <div>
-              <PortText variant="portHeadingText">{data?.title}</PortText>
+              <PortText variant="portHeadingText" className="portHeadingText">
+                {data?.title}
+              </PortText>
             </div>
             <div className="content">
-              <div>
-                <h3 className="head">Role</h3>
-                <p className="detail">{data?.role}</p>
-              </div>
-              <div>
-                <h3 className="head">Project Categories</h3>
-                {data?.category?.map(y => (
-                  <p className="detail">{y?.title}</p>
-                ))}
-              </div>
-              <h3 className="head">Technology Used</h3>
+              <h3 className="head">Role</h3>
+              <p className="detail">{data?.role}</p>
+              <h3 className="head">Project Category</h3>
+              {data?.category?.map(y => (
+                <p className="detail">{y?.title}</p>
+              ))}
+              <h3 className="head">Technologies Used</h3>
               {data?.technologyUsed?.map(y => (
                 <a aria-label="technology site" role="button" href={y?.url} className="detail">
                   <Button aria-label="technology">{y?.title}</Button>
@@ -51,8 +87,6 @@ const Project = ({ data }) => {
             </div>
           </div>
         </div>
-
-        {/* <Contact /> */}
       </div>
     </>
   );
