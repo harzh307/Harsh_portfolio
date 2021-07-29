07d5b7bebe105262dd577ae8fdbbf91fdca0968e
harzh307 <harzh.patel@gmail.com>
2021-07-29 16:10:12 +0530

    commit code for carousel

--- container/SinglePortfolio/index.js
+++ container/SinglePortfolio/index.js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { Fragment } from 'react';
+import { Fragment, useCallback, useEffect, useRef } from 'react';
 // import { useCallback, useEffect, useRef } from 'react';
 import Button from '../../component/Button';
 import Header from '../../component/Header';
@@ -8,34 +8,35 @@ import PortText from '../../component/PortText';
 
 const Project = ({ data }) => {
   const router = useRouter();
-  // const imgRef = useRef(null);
-  // const intervalRef = useRef();
-  // const cardRef = useRef();
+  const imgRef = useRef(null);
+  const intervalRef = useRef();
+  const cardRef = useRef([]);
 
-  // const startScroll = useCallback(() => {
-  //   intervalRef.current = setInterval(() => {
-  //     const total = imgRef.current.scrollLeft + imgRef.current.offsetWidth;
-  //     if (Math.round(total) === imgRef.current.scrollWidth) {
-  //       imgRef.current.scrollLeft = 0;
-  //     } else {
-  //       imgRef.current.scrollLeft += cardWidth;
-  //     }
-  //   }, 3000);
-  // }, []);
+  const startScroll = useCallback(() => {
+    intervalRef.current = setInterval(() => {
+      const cardWidth = cardRef.current[0].offsetWidth;
+      const total = imgRef.current.scrollLeft + imgRef.current.offsetWidth;
+      if (Math.round(total) === imgRef.current.scrollWidth) {
+        imgRef.current.scrollLeft = 0;
+      } else {
+        imgRef.current.scrollLeft += cardWidth;
+      }
+    }, 2000);
+  }, []);
 
-  // const stopScroll = useCallback(() => {
-  //   clearInterval(intervalRef.current);
-  // }, []);
+  const stopScroll = useCallback(() => {
+    clearInterval(intervalRef.current);
+  }, []);
 
-  // useEffect(() => {
-  //   startScroll();
-  //   imgRef.current.addEventListener('mouseover', stopScroll);
-  //   imgRef.current.addEventListener('mouseout', startScroll);
-  //   return () => {
-  //     imgRef.current.removeEventListener('mouseover', stopScroll);
-  //     imgRef.current.removeEventListener('mouseout', startScroll);
-  //   };
-  // }, [startScroll, stopScroll]);
+  useEffect(() => {
+    startScroll();
+    imgRef.current.addEventListener('mouseover', stopScroll);
+    imgRef.current.addEventListener('mouseout', startScroll);
+    return () => {
+      imgRef.current.removeEventListener('mouseover', stopScroll);
+      imgRef.current.removeEventListener('mouseout', startScroll);
+    };
+  }, [startScroll, stopScroll]);
 
   return (
     <>
@@ -54,16 +55,17 @@ const Project = ({ data }) => {
           </button>
         </Header>
         <div className="mainDiv">
-          <div className="imgdiv">
-            {/* <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" />
-            <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" />
-            <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" />
-            <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" />
-            <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" />
-            <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" />
-            <img src="../../WhatsApp Image 2021-06-18 at 12.25.39.jpeg" alt="project" /> */}
-            {data?.media?.map(x => (
-              <img src={x?.url} className="img" alt="projects" />
+          <div className="imgdiv" ref={imgRef}>
+            {data?.media?.map((x, i) => (
+              <img
+                src={x?.url}
+                key={x.id}
+                ref={el => {
+                  cardRef.current[i] = el;
+                }}
+                className="img"
+                alt="projects"
+              />
             ))}
           </div>
           <div className="textDiv">
